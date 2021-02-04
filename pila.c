52bc66d72c3498221e2035f917334dbf1e7deaf4
Emiliano <efs_070@ciencias.unam.mx>
2021-02-03 21:55:07 -0600

    Integracion de PIla

---
+++ pila.c
@@ -0,0 +1,76 @@
+// C program for array implementation of stack
+#include <stdio.h>
+#include <stdlib.h>
+#include "tokens.h"
+
+// A structure to represent a stack
+struct Stack {
+    int top;
+    int cantidad;
+    void **array;
+};
+
+// function to create a stack of given capacity. It initializes size of
+// stack as 0
+struct Stack* createStack()
+{
+    struct Stack* stack = (struct Stack*)malloc(sizeof(struct Stack));
+    stack->cantidad = 0;
+    stack->top = -1;
+    stack->array = malloc(sizeof(*stack->array));
+    return stack;
+}
+
+// Stack is empty when top is equal to -1
+int isEmpty(struct Stack* stack)
+{
+    return stack->cantidad == 0;
+}
+
+// Function to add an item to stack.  It increases top by 1
+void push(struct Stack* stack, const void *item)
+{
+    stack->array[++stack->top] = item;
+    stack->cantidad = ++(stack->cantidad) ;
+    printf("%p pushed to stack\n", item);
+}
+
+// Function to remove an item from stack.  It decreases top by 1
+void* pop(struct Stack* stack)
+{
+    if (isEmpty(stack))
+        return NULL;
+    stack->cantidad = --(stack->cantidad) ;
+    return stack->array[stack->top--];
+}
+
+// Function to return the top from stack without removing it
+void* peek(struct Stack* stack)
+{
+    if (isEmpty(stack))
+        return NULL;
+    return stack->array[stack->top];
+}
+
+int tamano(struct Stack* stack){
+  return stack->cantidad;
+}
+
+int main()
+{
+    struct Stack* stack = createStack();
+    char* ayuda = "1234";
+    struct token* symb = crea_token(COMA,ayuda);
+
+    push(stack,symb);
+
+    printf("%p peek \n", peek(stack));
+    printf("%d tamano \n", tamano(stack));
+    printf("%d vacio?\n", isEmpty(stack) );
+    pop(stack);
+    printf("%p peek \n", peek(stack));
+    printf("%d tamano \n", tamano(stack) );
+    printf("%d vacio?\n", isEmpty(stack) );
+
+    return 0;
+}
